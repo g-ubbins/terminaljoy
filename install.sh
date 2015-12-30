885f02dfbc82fc109616eba93bdc391e32e9d49d
Dave Gittins <dave.gittins@gmail.com>
2015-12-30 16:18:45 +1100

    Simpler install.sh for older bash

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ function die()
 function link()
 {
     if [[ -h $1 ]]; then
-        if [[ ! -v FORCE ]]; then
+        if ((FORCE)); then
             die "Not replacing existing symlink $1"
         fi
         rm -f $1 || die "Can't remove existing symlink $1"
         echo "Removed existing symlink $1"
     elif [[ -f $1 ]]; then
-        if [[ ! -v FORCE ]]; then
+        if ((FORCE)); then
             die "Not replacing existing file $1"
         fi
         mv $1 $1.bak || die "Can't move $1 to $1.bak"
@@ -42,7 +42,7 @@ function linkdot()
 while (($#)); do
     case $1 in
         -f)
-            FORCE=true
+            FORCE=1
             ;;
         *)
             die "Unknown argument $1"
